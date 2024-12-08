0527742c014661fc7a6cae569cfcc81fa77fd774
Eddyzzzzz <eddyzyf@gmail.com>
2024-12-08 01:04:23 -0500

    Last Update in 2024. Merry Christmas!

--- _nuxt/static/1724014465/introduction/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1724014465",layout:"default",error:a,state:{language:{language:"en"},size:{height:b,width:b},i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fintroduction",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,0));
--- _nuxt/static/1724014465/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1724014465",layout:"default",error:a,state:{language:{language:"en"},size:{height:b,width:b},i18n:{routeParams:{}}},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,0,"\u002F"));
